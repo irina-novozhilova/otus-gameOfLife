435279effbc74988067179ca5ba85f940d6e938c
Irina Novozhilova <irina@nsvision.com>
2023-04-09 13:24:56 +0400

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -43,9 +43,6 @@ module.exports = (env) => ({
         test: /\.ts?$/,
         use: "ts-loader",
         exclude: /node_modules/,
-        options: {
-          configFile: path.resolve("./tsconfig.json"),
-        },
       },
       {
         test: /\.css$/i,
