505f23bab6734e01c4c599e956a35fbce5f1bc2c
irina-novozhilova <irina-novozhilova@users.noreply.github.com>
2023-04-09 09:34:22 +0000

    Deploying to gh-pages from @ irina-novozhilova/otus-gameOfLife@0716c486fb4a3fb79022da6d598bfd5f990e31d2 🚀

---
+++ index-f84b3013ebf856e4a789.js
@@ -0,0 +1,17 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./build/src/x.js
+function x(n) {
+  console.log("x with n", n);
+}
+;// CONCATENATED MODULE: ./build/src/index.js
+
+function y() {
+  x(1);
+}
+y();
+/******/ })()
+;
+//# sourceMappingURL=index-f84b3013ebf856e4a789.js.map
